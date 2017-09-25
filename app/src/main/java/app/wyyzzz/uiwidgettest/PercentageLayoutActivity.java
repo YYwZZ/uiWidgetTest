a810cec0bed181edc42ca8e8cbe5e541f4b19426
YYwZZ <1195071851@qq.com>
2017-09-25 15:22:52 +0800

    bar

---
+++ app/src/main/java/app/wyyzzz/uiwidgettest/PercentageLayoutActivity.java
@@ -0,0 +1,13 @@
+package app.wyyzzz.uiwidgettest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PercentageLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_percentage_layout);
+    }
+}
